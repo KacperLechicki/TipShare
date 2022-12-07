5907e70d217a16c00dcae0db458d8bf99041cf3d
Kacper Lechicki <klechicki@poczta.fm>
2022-12-07 13:41:18 +0100

    js checking inputs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const billInput = document.querySelector('.tipShare__top-input');
+const peopleInput = document.querySelector('.people-input');
+const tips = document.querySelector('select');
+const error = document.querySelector('.tipShare__bottom-error');
+const calcBtn = document.querySelector('.tipShare__bottom-calc');
+const costInfo = document.querySelector('.tipShare__bottom-costInfo');
+const cost = document.querySelector('.tipShare__bottom-cost');
+
+error.style.visibility = 'hidden';
+
+console.log(peopleInput.value);
+
+const validate = () => {
+	if (
+		billInput.value == '' ||
+		peopleInput.value == '' ||
+		tips.value == '0'
+	) {
+		error.textContent = 'Uzupełnij wszystkie dane!';
+		error.style.visibility = 'visible';
+	} else {
+		error.style.visibility = 'hidden';
+	}
+};
+
+calcBtn.addEventListener('click', validate);
