84e8b499eeaad839f8b6e38e799ef9a29656b195
Kacper Lechicki <klechicki@poczta.fm>
2022-12-07 13:56:07 +0100

    ready

--- js/script.js
+++ js/script.js
@@ -7,22 +7,38 @@ const error = document.querySelector('.tipShare__bottom-error');
 const calcBtn = document.querySelector('.tipShare__bottom-calc');
 const costInfo = document.querySelector('.tipShare__bottom-costInfo');
 const cost = document.querySelector('.tipShare__bottom-cost');
+const resetBtn = document.querySelector('.tipShare__bottom-reset');
 
 error.style.visibility = 'hidden';
 
 console.log(peopleInput.value);
 
 const validate = () => {
-	if (
-		billInput.value == '' ||
-		peopleInput.value == '' ||
-		tips.value == '0'
-	) {
+	if (billInput.value == '' || peopleInput.value == '' || tips.value == '0') {
 		error.textContent = 'Uzupełnij wszystkie dane!';
 		error.style.visibility = 'visible';
+		cost.textContent = '';
 	} else {
 		error.style.visibility = 'hidden';
+		calc();
 	}
 };
 
+const calc = () => {
+	const bill = parseFloat(billInput.value);
+	const people = parseInt(Math.floor(peopleInput.value));
+	const tip = parseFloat(tips.value);
+
+	const price = (bill + bill * tip) / people;
+	cost.textContent = price.toFixed(2).toString();
+};
+
+const resetInputs = () => {
+	billInput.value = '';
+	peopleInput.value = '';
+	tips.value = '0';
+    cost.textContent = '';
+};
+
 calcBtn.addEventListener('click', validate);
+resetBtn.addEventListener('click', resetInputs);
